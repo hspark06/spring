44f3bc3a131b078e7d55db58aff36ab724357adb
hspark06 <hspark06@gmail.com>
2021-04-27 16:04:00 +0900

    test

---
+++ src/main/java/com/ennio/main/chapter1/UserDaoTest.java
@@ -0,0 +1,25 @@
+package com.ennio.main.chapter1;
+
+import com.ennio.main.chapter1.dao.UserDao;
+import com.ennio.main.chapter1.domain.User;
+import java.sql.SQLException;
+
+public class UserDaoTest {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		UserDao dao = new UserDao();
+
+		User user = new User();
+		user.setId("whiteship");
+		user.setName("백기선");
+		user.setPassword("married");
+
+		dao.add(user);
+
+		System.out.println(user.getId() + " 등록 성공");
+
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		System.out.println(user2.getId() + " 조회 성공");
+	}
+}
