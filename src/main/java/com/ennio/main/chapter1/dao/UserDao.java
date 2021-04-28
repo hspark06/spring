a15acdd35dd011ca398768b421a689cb6c9c5b65
hspark06 <hspark06@gmail.com>
2021-04-28 10:34:41 +0900

    test

--- src/main/java/com/ennio/main/chapter1/dao/UserDao.java
+++ src/main/java/com/ennio/main/chapter1/dao/UserDao.java
@@ -8,9 +8,7 @@ public class UserDao {
    
     public void add(User user) throws ClassNotFoundException, SQLException {
 		
-        Class.forName("org.h2.Driver");
-		Connection c = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "user", "dsic");
-
+        Connection c = getConnection();
 		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values (?, ?, ?)");
 		ps.setString(1, user.getId());
 		ps.setString(2, user.getName());
@@ -23,8 +21,7 @@ public class UserDao {
 	}
 
 	public User get(String id) throws ClassNotFoundException, SQLException {
-		Class.forName("org.h2.Driver");
-		Connection c = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "user", "dsic");
+		Connection c = getConnection();
 
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
@@ -42,5 +39,9 @@ public class UserDao {
 		c.close();
 
 		return user;
+	}
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("org.h2.Driver");
+		return DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "user", "dsic");
 	}
 }
