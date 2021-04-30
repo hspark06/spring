ec5a285927fea7dcc98a3c80d43b39d0a4bf3bff
hspark06 <hspark06@gmail.com>
2021-04-30 16:56:05 +0900

    .

--- src/main/java/com/ennio/main/chapter1/dao/UserDao.java
+++ src/main/java/com/ennio/main/chapter1/dao/UserDao.java
@@ -5,6 +5,8 @@ import javax.sql.DataSource;
 
 import com.ennio.main.chapter1.domain.User;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+
 public class UserDao {
    
 	private DataSource dataSource;
@@ -33,17 +35,22 @@ public class UserDao {
 		PreparedStatement ps = c
 				.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
-
+		
 		ResultSet rs = ps.executeQuery();
-		rs.next();
-		User user = new User();
-		user.setId(rs.getString("id"));
-		user.setName(rs.getString("name"));
-		user.setPassword(rs.getString("password"));
+
+		User user = null;
+		if (rs.next()) {
+			user = new User();
+			user.setId(rs.getString("id"));
+			user.setName(rs.getString("name"));
+			user.setPassword(rs.getString("password"));
+		}
 
 		rs.close();
 		ps.close();
 		c.close();
+		
+		if (user == null) throw new EmptyResultDataAccessException(1);
 
 		return user;
 	}
--- src/test/java/com/ennio/main/UserDaoTest.java
+++ src/test/java/com/ennio/main/UserDaoTest.java
@@ -1,6 +1,11 @@
 package com.ennio.main;
 
+import org.h2.jdbc.JdbcSQLNonTransientException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -9,45 +14,52 @@ import com.ennio.main.chapter1.dao.UserDao;
 import com.ennio.main.chapter1.domain.User;
 import java.sql.SQLException;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class) 
+@ContextConfiguration(locations="/com/ennio/main/chapter1/applicationContext.xml")
 @SpringBootTest
 public class UserDaoTest {
 	
+	@Autowired
+	private UserDao dao; 
+	
+	private static User user1;
+	private static User user2;
+	private static User user3;
+
+	@BeforeAll
+	static public void setUp() {
+		
+		user1 = new User("gyumee", "111", "springno1");
+		user2 = new User("leegw700", "222", "springno2");
+		user3 = new User("bumjin", "333", "springno3");
+	}
+
 	@Test
 	public void andAndGet() throws SQLException {	
-
-		//AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-		ApplicationContext context = new GenericXmlApplicationContext("com/ennio/main/chapter1/applicationContext.xml");
-		UserDao dao = context.getBean("userDao", UserDao.class);
-
+		
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
-		
-		User user = new User();
-		user.setId("gyumee");
-		user.setName("1111");
-		user.setPassword("springno1");
 
-		dao.add(user);
-		assertThat(dao.getCount(), is(1));
+		dao.add(user1);
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
 		
-		User user2 = dao.get(user.getId());
+		User userget1 = dao.get(user1.getId());
+		assertThat(userget1.getName(), is(user1.getName()));
+		assertThat(userget1.getPassword(), is(user1.getPassword()));
 		
-		assertThat(user2.getName(), is(user.getName()));
-		assertThat(user2.getPassword(), is(user.getPassword()));
+		User userget2 = dao.get(user2.getId());
+		assertThat(userget2.getName(), is(user2.getName()));
+		assertThat(userget2.getPassword(), is(user2.getPassword()));
+
 	}
 
 	@Test
 	public void count() throws SQLException {
-		ApplicationContext context = new GenericXmlApplicationContext("com/ennio/main/chapter1/applicationContext.xml");
-		
-		UserDao dao = context.getBean("userDao", UserDao.class);
-		User user1 = new User("gyumee", "박성철", "springno1");
-		User user2 = new User("leegw700", "이길원", "springno2");
-		User user3 = new User("bumjin", "박범진", "springno3");
-				
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
 				
@@ -60,4 +72,17 @@ public class UserDaoTest {
 		dao.add(user3);
 		assertThat(dao.getCount(), is(3));
 	}
+	
+	@Test
+	public void getUserFailure() throws SQLException {
+		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			dao.get("unknown_id");
+		  });
+	}
+
 }
